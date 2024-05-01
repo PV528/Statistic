b3bae73882d057314a72eb23c7c89b21167a8ce6
PV <peter.vezjak@student.um.si>
2024-05-02 00:32:14 +0200

    dockerhub

--- statistic.js
+++ statistic.js
@@ -16,18 +16,18 @@ mongoose.connect('mongodb://mongologs:27017/logs', {
 const Log = require('./models/log');
 
 app.get('/endpoint-counts', async (req, res) => {
-  try {
-    const counts = await Log.aggregate([
-      { $group: { _id: '$url', count: { $sum: 1 } } }
-    ]);
-
-    res.json(counts);
-  } catch (error) {
-    console.error('Error counting endpoints:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
+    try {
+      const counts = await Log.aggregate([
+        { $group: { _id: { url: '$url', method: '$method' }, count: { $sum: 1 } } }
+      ]);
+  
+      res.json(counts);
+    } catch (error) {
+      console.error('Error counting endpoints:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
 app.listen(PORT, () => {
   console.log(`Microservice is running on port ${PORT}`);
 });
